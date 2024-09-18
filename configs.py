95369e70cf55b9b0a460ad23cbcb738b4f804d75
Soran Ghaderi <sg23454@essex.ac.uk>
2024-09-18 16:31:35 +0000

    feat: evaluation ready
    1. including 3 selected layers for integration
    2. over mbpp (250 instances)
    3. generates and parses outputs and add their respective columns
    4. checkpoint layer_idx and instance_idx and json of results
    5. handles out of memory
    6. save/load checkpoint

--- configs.py
+++ configs.py
@@ -6,13 +6,15 @@ MAX_LENGTH = 512
 CRV_DIM = 4096  # LLaMA hidden size -> should derive it from the model configs
 SUBSET_SIZE = 7
 CRV_SAVE_BATCH = 20
-CRV_LAYERS = [
-    1,
-    5,
-    10,
-    15,
-    20,
-    32,
-]  # the hidden states include the input embedding layer therefore idx 0 is the embeds lyr
+# CRV_LAYERS = [
+#     1,
+#     5,
+#     10,
+#     15,
+#     20,
+#     32,
+# ]  # the hidden states include the input embedding layer therefore idx 0 is the embeds lyr
+
+CRV_LAYERS = [1, 10, 23, "orig"]
 USE_RICH = False
 logging_level = logging.ERROR
